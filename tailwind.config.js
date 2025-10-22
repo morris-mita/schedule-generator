9289d1a01a5f6e37f0fb098f23a09f4743a14bee
morris-mita <6jwj4r9xjf@privaterelay.appleid.com>
2025-10-21 21:15:03 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6366f1',
+          dark: '#5558e3',
+        },
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
